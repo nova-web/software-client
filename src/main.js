6fc18f8975b43da0072d0633dc26cf5a65ee0a00
nienie0714 <niexinyu0714@163.com>
2018-11-16 17:24:08 +0800

    App.vue  lang="less"

--- src/main.js
+++ src/main.js
@@ -1,12 +1,9 @@
 import Vue from 'vue';
+import App from './App';
 import router from './router';
 import ElementUI from 'element-ui';
-import App from './App';
-// 引入style文件, 覆盖element-ui样式
-require('!style-loader!css-loader!less-loader!./style/index.less');
 import store from './store';
 import 'element-ui/lib/theme-chalk/index.css'; // 默认主题
-// import '../static/css/theme-green/index.css'; // 浅绿色主题
 import 'babel-polyfill';
 import Blob from './utils/Blob.js';
 import Export2Excel from './utils/Export2Excel.js';
